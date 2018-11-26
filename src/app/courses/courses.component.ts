3e3da0f8c74d8cfda24fc67cb9f7688a64134dca
Alekhya Vadali <alekhyavadali@Alekhyas-Air.fios-router.home>
2018-11-26 03:46:04 -0500

    search implemented

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from '../data.service';
 import { RouterModule, Router } from '@angular/router';
+import { CardModel } from '../card.model';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
@@ -10,9 +11,23 @@ export class CoursesComponent implements OnInit {
 
   constructor(private dataService: DataService, private router: Router) { }
   ngOnInit() {
+    this.filterItem('');
   }
+  filteredItems: CardModel[] = null;
 clickvideo(video){
 this.dataService.clickedCourse = video;
 this.router.navigateByUrl('videos');
 }
+filterItem(input) {debugger;
+  if(input ==null || input == undefined) {
+    this.filteredItems = this.dataService.courses;
+  }
+  else {
+    this.filteredItems = [];
+this.filteredItems = this.dataService.courses.filter(CardModel => CardModel.cardtitle.includes(input));
+debugger;
+console.log(this.filteredItems);
+  }
+}
+
 }
